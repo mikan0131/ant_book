1a172b3d5389bc019bd22dfce0224e4495fc1979
mikan0131 <kotaro9111@gmail.com>
2024-09-18 17:06:46 +0900

    2_1_2.cpp add(not complete)

---
+++ 2_1_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+char W[109][109];
+
+void dfs(int i, int j) {
+    
+}
+
+int main() {
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) cin >> W[i][j];
+    }
+}
