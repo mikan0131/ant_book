488b14165820029803ef4f9160917c3b00b5b2e6
mikan0131 <kotaro9111@gmail.com>
2024-09-18 16:29:17 +0900

    1_6_2.cpp correct!

--- 1_6_2.cpp
+++ 1_6_2.cpp
@@ -2,25 +2,18 @@
 #include <algorithm>
 using namespace std;
 
-const int INF = 1'000'000'000;
+int L, N, X[100009];
 
 int main() {
-    int T;
-    cin >> T;
-    for (int i = 0; i < T; i++) {
-        int L, N, X[1000009];
-        cin >> L >> N;
-        for (int j = 0; j < N; j++) cin >> X[i];
-        int m = INF;
-        for (int j = 0; j < N; j++) {
-            m = min(m, X[j] - 0);
-            m = min(m, L - X[j]);
-        }
-        int M = 0;
-        for (int j = 0; j < N; j++) {
-            M = max(M, X[j] - 0);
-            M = max(M, L - X[j]);
-        }
-        cout << m << ' ' << M << endl;
+    cin >> L >> N;
+    for (int i = 0; i < N; i++) cin >> X[i];
+    int minT = 0;
+    for (int i = 0; i < N; i++) {
+        minT = max(minT, min(X[i], L - X[i]));
     }
+    int maxT = 0;
+    for (int i = 0; i < N; i++) {
+        maxT = max(maxT, max(X[i], L - X[i]));
+    }
+    printf("%d %d\n", minT, maxT);
 }
