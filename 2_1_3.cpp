beb9113f8bd616359cfee7bc022ed594a399c99a
mikan0131 <kotaro9111@gmail.com>
2024-10-05 12:20:16 +0900

    2_1_3 completed

--- 2_1_3.cpp
+++ 2_1_3.cpp
@@ -2,36 +2,43 @@
 #include <queue>
 using namespace std;
 
-const int INF = 1'000'000'000;
-typedef pair <int, int> P;//typedefしとくとあとで便利
-
-char maze[109][109];
+typedef pair <int, int> P;
+char maze[19][19 + 1];
 int N, M;
 int sx, sy;
 int gx, gy;
-
-int d[109][109];
-
-//方向のベクトル
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
+const int INF = 1'000'000'000;
+int d[19][19];
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 
 int dfs() {
-    queue<P> que;
+    queue <P> que;
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            d[i][j] = INF;
-        }
+        for (int j = 0; j < M; j++) cin >> maze[i][j];
     }
     que.push(P(sx, sy));
     d[sx][sy] = 0;
-    while (que.size()) {//キューが空になるまで続ける
+    while (que.size()) {
         P p = que.front(); que.pop();
         if (p.first == gx && p.second == gy) break;
         for (int i = 0; i < 4; i++) {
-            int nx = p.first + dx[i]; int ny = p.second + dy[i];
-            if (0 <= nx && nx < N && 0 <= ny && ny < M && maze[nx][ny] == '#' && d[nx][ny] == INF) que.push(P(nx, ny));
-            
+            int nx = p.first + dx[i], ny = p.second + dy[i];
+            if (0 <= nx && nx < N && 0 <= ny && ny < M && maze[nx][ny] != '#' &&d[nx][ny] == INF) {
+                que.push(P(nx, ny));
+                d[nx][ny] = d[p.first][p.second] + 1;
+            }
         }
     }
+    return d[gx][gy];
+}
+
+int main() {
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> maze[i][j];
+        }
+    }
+    printf("%d\n", dfs());
+    return 0;
 }
