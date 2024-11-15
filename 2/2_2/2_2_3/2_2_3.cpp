54a7f9fc703245b70c8503694fddb50b3b00645c
mikan0131 <kotaro9111@gmail.com>
2024-11-15 16:50:48 +0900

    2_2_3 complete

--- 2/2_2/2_2_3/2_2_3.cpp
+++ 2/2_2/2_2_3/2_2_3.cpp
@@ -1,3 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N;
+char S[2009];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> S[i];
+    int a = 0, b = N - 1;
+    while (a <= b) {
+        bool left = false;
+        for (int i = 0; a + i <= b; i++) {
+            if (S[a + i] < S[b - i]) {
+                left = true;
+                break;
+            }
+            else if (S[a + i] > S[b - i]) {
+                left = false;
+                break;
+            }
+        }
+        if (left) putchar(S[a++]);
+        else putchar(S[b--]);
+    }
+    putchar('\n');
+    return 0;
+}
